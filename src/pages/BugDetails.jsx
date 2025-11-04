638094d5d7180597554a96b7e0d510801c2254aa
oxanashva <oxanashav@gmail.com>
2025-11-04 11:09:02 +0200

    feat: restrict unauthorized users to viewing only 3 bugs

--- src/pages/BugDetails.jsx
+++ src/pages/BugDetails.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom'
 
 
 export function BugDetails() {
-
+    const [limitExceeded, setLimitExceeded] = useState(false)
     const [bug, setBug] = useState(null)
     const { bugId } = useParams()
 
@@ -21,12 +21,28 @@ export function BugDetails() {
             const bug = await bugService.getById(bugId)
             setBug(bug)
         } catch (err) {
-            showErrorMsg('Cannot load bug')
-
+            if (err.response.status === 401) {
+                setLimitExceeded(true)
+                showErrorMsg(err.response.data)
+            } else {
+                showErrorMsg('Cannot load bug')
+            }
         }
     }
 
+    if (limitExceeded) {
+        return (
+            <div className="bug-details main-layout">
+                <h2>Access Denied 🚫</h2>
+                <p>You must be authorized to view this bug.</p>
+                <p><Link to="/bug">Back to List</Link></p>
+            </div>
+        )
+    }
+
+
     if (!bug) return <h1>loadings....</h1>
+
     return <div className="bug-details main-layout">
         <h3>Bug Details 🐛</h3>
         <h4>{bug.title}</h4>
